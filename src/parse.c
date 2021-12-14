6a6fe1c455b9b3a90c1a8d50f929a668867de5f0
gyujun9403 <gujun112@naver.com>
2021-12-14 13:14:01 +0900

    doing parsing

--- src/parse.c
+++ src/parse.c
@@ -3,17 +3,23 @@
 
 #define	FLG_ON 1
 #define	FLG_OFF 0
-#define FLG_NQ -2	// No Quotes FLG ON
-#define FLG_AQ -1	// ALL Quotes FLG ON
-#define	FLG_SQ 0	// Single Quotes / Single Quotes FLG ON
-#define	FLG_DQ 1	// Double Quotes / Double Quotes FLG ON
-#define FLG_BS 2	// Backslash / Backslash FLG ON
-#define FLG_DL 3	// Dolloar sign / Dolloar sign FLG ON
+#define FLG_NQ	0b0000	// No Quotes FLG ON
+#define MSK_NQ	0b1111	// Mask for checking single quotes
+#define	FLG_SQ	0b0001	// Single Quotes / Single Quotes FLG ON
+#define	FLG_DQ	0b0010	// Double Quotes / Double Quotes FLG ON
+#define FLG_BS 0b0100	// Backslash / Backslash FLG ON
+#define FLG_DL 0b1000	// Dolloar sign / Dolloar sign FLG ON
+
+#define TYPE_UNDEFINED	0
+
+int ft_isWhite(char c) {
+	return ((c >= 9 && c <= 13) || c == 32);
+}
 
 t_ast	*paser(char *line) {
 	// flgs[0] : '', flg[1] : ""
 	// flg[3] : \, flg[4] : $
-	char 	flgs[2];
+	char 	flgs;
 	t_ast 	*result;
 	t_ast	*ptr_result;
 	int		idx;
@@ -21,57 +27,48 @@ t_ast	*paser(char *line) {
 
 	idx = 0;
 	slide = 0;
-	flgs[0] = 0;
-	flgs[1] = 0;
+	flgs = 0;
 	result = excep_malloc(sizeof(t_ast));
 	ptr_result = result;
+	ptr_result->type = TYPE_UNDEFINED;
 	// 밑에 구분은 gnl처럼 더 읽을게 없을때까지 NULL을 반환하는...?
 	// 아니다 차라리 반환에만 쓰이는 struct를 만들어서
 	// 상태정보 + 변환된 문자열을 계속출력하게
+	/* Case1. qutos가 아무것도 ON 되지 않았을 때
+ *		qutos가 아닐때	: slide를 하나 올리고 다음 글자로.
+ *			이때, |, ;가 오면 끊어줘야한다...
+ * 		signle quote일때	: flg[0]을 ON으로 만들고 다음 글자.
+ * 		double quote일때	: flg[1]을 ON으로 만들고 다음 글자.
+ * 			이때, $, \가 오면 이를 치환해주여야한다. -> 일단
+ * Case2. signle quote가 ON 되었을 때
+ * 		qutos가 아닐때	: slide를 하나 올리고 다음 글자로, 이때 모든 특수문자는 무시.
+ * 		signle quote일때 : flg[0]을 OFF으로 만들고 다음 글자.
+ * 		double quote일때	: 일반문자로 인식하여 다음문자.
+ * Case3. double quote가 ON 되었을 때
+ * 		qutos가 아닐때	: slide를 하나 올리고 다음 글자로
+ * 		signle quote일때	: 일반문자로 인식하여 다음문자.
+ * 		double quote일때 : flg[1]을 OFF으로 만들고 다음 글자.
+*/
+	/*
+	 * strlcat을 사용하는 방법
+	 * ', "플래그가 없으면 space,',"가 나올때까지 길이를 잰 다음에 "\0"만 있는 문자열에 strlcat(빈문자열, 시작위치, 길이)을 한다.
+	 * '플래그가 있으면 '만날때까지 길이를 재고 strlcat하되, \를 만나고 특정 조건을 만족하면 \전까지 끊어서 strlcat한 다음,
+	 */
 	while (line[idx] != '\0') {
-		while (line[idx] != '\0') {
-			if (line[idx++] != ' ')
-				break ;
-		}
+		while (ft_isWhite(line[idx]))
+			++idx;
+		// 한 글자씩 처리
 		while (line[idx + slide]) {
-			/* Case1. qutos가 아무것도 ON 되지 않았을 때
-			 *		qutos가 아닐때	: slide를 하나 올리고 다음 글자로.
-			 *			이때, |, ;가 오면 끊어줘야한다...
-			 * 		signle quote일때	: flg[0]을 ON으로 만들고 다음 글자.
-			 * 		double quote일때	: flg[1]을 ON으로 만들고 다음 글자.
-			 * 			이때, $, \가 오면 이를 치환해주여야한다. -> 일단
-			 * Case2. signle quote가 ON 되었을 때
-			 * 		qutos가 아닐때	: slide를 하나 올리고 다음 글자로, 이때 모든 특수문자는 무시.
-			 * 		signle quote일때 : flg[0]을 OFF으로 만들고 다음 글자.
-			 * 		double quote일때	: 일반문자로 인식하여 다음문자.
-			 * Case3. double quote가 ON 되었을 때
-			 * 		qutos가 아닐때	: slide를 하나 올리고 다음 글자로
-			 * 		signle quote일때	: 일반문자로 인식하여 다음문자.
-			 * 		double quote일때 : flg[1]을 OFF으로 만들고 다음 글자.
-			*/
-			/*
-			 * strlcat을 사용하는 방법
-			 * ', "플래그가 없으면 space,',"가 나올때까지 길이를 잰 다음에 "\0"만 있는 문자열에 strlcat(빈문자열, 시작위치, 길이)을 한다.
-			 * '플래그가 있으면 '만날때까지 길이를 재고 strlcat하되, \를 만나고 특정 조건을 만족하면 \전까지 끊어서 strlcat한 다음,
-			 */
-			if (flgs[FLG_SQ] == FLG_ON) {
-				while (line[idx + slide] == '\'') {
-					if (line[idx + slide] == '\\') {
-						if (flgs[FLG_BS] == FLG_ON)
-							; // \\를 그대로 출력하게...
-						else
-							flgs[FLG_BS] = FLG_ON;
-					}
-					++slide;
-				}
-			}
-			else if (flgs[FLG_DQ] == FLG_ON) {
-				while (line[idx + slide++] == '\"');
-				// 특수문자를 변환하는
-			}
-			if (line[idx + slide] == ' ')
+			if ((flgs & MSK_NQ) == FLG_SQ) {
+				if (line[idx + slide] == ' ') {
+					if (ptr_result->type == TYPE_UNDEFINED) {
 
-			++slide;
+					}
+				}
+				else
+					++slide;
+			}
+
 		}
 	}
 
