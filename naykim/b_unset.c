ba1590b01d3c64ad68e21a79737fa53eeaa13f49
nayeon <vennie20@naver.com>
2021-12-30 14:07:15 +0900

    naykim export, unset

--- naykim/b_unset.c
+++ naykim/b_unset.c
@@ -1,2 +1,42 @@
 #include "../header/minishell.h"
 
+char **b_unset(char **origin, char *remove)
+{
+	char	**new;
+	int		idxlen;
+	int		i;
+	int		newi;
+	int		flag = 0;
+
+	if (!origin)
+		return (NULL);
+	if (!remove)
+		return (NULL);
+	idxlen = 0;
+	while (origin[idxlen])
+		idxlen++;
+	i = 0;
+	while (i < idxlen)
+	{
+		if (ft_strncmp(origin[i], remove, ft_strlen(remove)))
+			flag = 1;
+		i++;
+	}
+	if (flag == 1)
+		new = (char **)malloc(sizeof(char *) * idxlen);
+	else
+		return (origin);
+	i = -1;
+	newi = 0;
+	while (++i < idxlen)
+	{
+		if (!ft_strncmp(origin[i], remove, ft_strlen(remove)))
+			new[newi++] = origin[i];
+		else
+			free(origin[i]);
+	}
+	new[idxlen] = 0;
+	free(origin);
+	origin = 0;
+	return (new);
+}
