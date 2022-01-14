3253e34ae6a853f450b31041ca3e6d34003d83fb
gyujun9403 <gujun112@naver.com>
2022-01-14 13:50:05 +0900

    Feat: leak 해결중

--- srcs/p_actions2.c
+++ srcs/p_actions2.c
@@ -13,8 +13,9 @@ char	action_addonestring(t_ast *lst, char **cursor)
 			lst = get_last(lst);
 			lst->text = ft_addonestring(lst->text, *cursor);
 			free(*cursor);
+			*cursor = NULL;
 		}
-		*cursor = NULL;
+		// *cursor = NULL;
 	}
 	return ('s');
 }
